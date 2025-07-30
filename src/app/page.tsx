8cf1f73617cb13e6902fd957a3dd96888f1caa89
Emilie Mutoniwabo <143102871+edmutoni@users.noreply.github.com>
2025-07-30 19:13:25 -0400

    adding button layout to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./globals.css";
 import image from "./images/sample.png";
+import { NavButton } from "./components/button";
 
 export default function Home() {
   return (
@@ -16,11 +17,11 @@ export default function Home() {
         <div className="content">
           <div>
             <main>
-              <h1 style={{ color: "black", fontSize: 36 }}>
-                hello, my name is emilie!
+              <h1 style={{ color: "black", fontSize: 48 }}>
+                hello, i'm emilie!
               </h1>
               <br></br>
-              <h2 style={{ color: "black", fontSize: 25 }}>
+              <h2 style={{ color: "black", fontSize: 36 }}>
                 welcome to my website
               </h2>
               <br></br>
@@ -33,6 +34,40 @@ export default function Home() {
             <img src="sample.png" alt="Sample Image" width={300} height={300} />
           </div>
         </div>
+        <div className="buttons-container">
+          <div className="button-single">
+            <div>
+              <img src="star.png" alt="about me" width={75} height={75} />
+            </div>
+            <div>
+              <p style={{ color: "black", fontSize: 18 }}>about me</p>
+            </div>
+          </div>
+          <div className="button-single">
+            <div>
+              <img src="star.png" alt="my work" width={75} height={75} />
+            </div>
+            <div>
+              <p style={{ color: "black", fontSize: 18 }}>my work</p>
+            </div>
+          </div>
+          <div className="button-single">
+            <div>
+              <img src="star.png" alt="contact me" width={75} height={75} />
+            </div>
+            <div>
+              <p style={{ color: "black", fontSize: 18 }}>contact me</p>
+            </div>
+          </div>
+          <div className="button-single">
+            <div className="resume-position">
+              <img src="star.png" alt="my resume" width={75} height={75} />
+              <div>
+                <p style={{ color: "black", fontSize: 15 }}>my resume</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <footer className="text-white p-10 text-center">
         Made with 🪼 by Emilie
@@ -40,61 +75,3 @@ export default function Home() {
     </div>
   );
 }
-
-/* export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-        <footer className="text-white p-4 text-center ">
-          Made with 🪼 by Emilie
-        </footer>
-      </main>
-    </div>
-  );
-} */
